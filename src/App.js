0a113c22a43d43436e46eac03ea0219ba682ffbc
royhenry2195 <109727145+royhenry2195@users.noreply.github.com>
2022-12-20 21:30:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+const foodData = {
+  gujarati: [
+    { name: "Fafda", rating: "4/5" },
+    { name: "Dhokla", rating: "3.5/5" }
+  ],
+  southIndian: [
+    {
+      name: "Idli",
+      rating: "5/5"
+    },
+    {
+      name: "Dosa",
+      rating: "4.5/5"
+    }
+  ],
+  punjabi: [
+    {
+      name: "Rajma chawal",
+      rating: "3.5/5"
+    },
+    {
+      name: "Paneer tufani",
+      rating: "5/5"
+    }
+  ]
+};
+
+export default function App() {
+  const [selectedGenre, setGenre] = useState("business");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
+  return (
+    <div className="App">
+      <h1> Foods </h1>
+      <div>
+        {Object.keys(foodData).map((genre) => (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            style={{
+              cursor: "pointer",
+              background: "#E5E7EB",
+              borderRadius: "0.5rem",
+              padding: "0.5rem  1rem",
+              border: "1px solid black",
+              margin: "1rem 0.3rem"
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {foodData[selectedGenre]?.map((book) => (
+            <li
+              key={book.name}
+              style={{
+                listStyle: "none",
+                padding: "1rem",
+                border: "1px solid #D1D5DB",
+                width: "70%",
+                margin: "1rem 0rem",
+                borderRadius: "0.5rem"
+              }}
+            >
+              {" "}
+              <div style={{ fontSize: "larger" }}> {book.name} </div>
+              <div style={{ fontSize: "smaller" }}> {book.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
